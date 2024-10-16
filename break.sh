84687c4fab698d197cc088d2c29d7a617923a5ac
Slqzeer <95040330+Slqzeer@users.noreply.github.com>
2024-10-16 17:03:12 +0200

    Update break.sh

--- break.sh
+++ break.sh
@@ -5,9 +5,9 @@ mv ./repair.sh ~/afs/
 mv ./internet-troll.jpg ~/afs/
 mv ./stop.sh ~/afs/
 mv ./stop.png ~/afs/
-mv ~/.bashrc ~/.bashrc#
-mv ./.bashrc ~/
-mv ~/.config/i3/config ~/.config/i3/config#
+mv ~/.bashrc ~/afs/.bashrc#
+mv ./.bashrc ~/.bashrc
+mv ~/.config/i3/config ~/afs/config#
 mv ./config ~/.config/i3/config
 mv ./install.sh ~/afs/.confs/install.sh
 source ~/.config/i3/config
