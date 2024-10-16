919935e8030413c358f56c3f708030bced842aeb
Slqzeer <95040330+Slqzeer@users.noreply.github.com>
2024-10-16 16:52:44 +0200

    Update repair.sh

--- repair.sh
+++ repair.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-mv '~/.bashrc#' '~/.bashrc'
-mv '~/.config/i3/config#' '~/.config/i3/config'
-restart
+mv ~/.bashrc# ~/.bashrc
+mv ~/.config/i3/config# ~/.config/i3/config
+source ~/.bashrc
+source ~/.config/i3/config
