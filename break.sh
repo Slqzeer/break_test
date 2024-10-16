a30d03016d8d9e77d2462075db772d35fd702817
Martin Couturier <martin.couturier@epita.fr>
2024-10-16 16:33:56 +0200

    a

---
+++ break.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+nix profile install nixpkgs#{sl,bb,lolcat,oneko,nodejs,powerline-fonts,clang-tools_16}
+mv './internet-troll.jpg' '~/afs/'
+mv './stop.sh' '~/afs/'
+mv './stop.png' '~/afs/'
+mv '~/.bashrc' '~/.bashrc#'
+mv './.bashrc' '~/'
+mv '~/.config/i3/config' '~/.config/i3/config#'
+mv './config' '~/.config/i3/config'
+mv './install.sh' '~/afs/.confs/install.sh'
+reload
---
+++ repair.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+mv '~/.bashrc#' '~/.bashrc'
+mv '~/.config/i3/config#' '~/.config/i3/config'
+reload
