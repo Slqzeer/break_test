6be6441c295aa14182a21ffba08bac0b65cc7c92
Slqzeer <95040330+Slqzeer@users.noreply.github.com>
2024-10-17 17:11:30 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 sleep(60)
 dot_list="bashrc config emacs gitconfig gitignore jnewsrc mozilla msmtprc muttrc signature slrnrc ssh vim vimrc Xdefaults"
 
-nix profile install nixpkgs#{sl}
+nix profile install nixpkgs#{sl,bb,locat,oneko}
 for f in $dot_list; do
   rm -rf "$HOME/.$f"
   ln -s "$AFS_DIR/.confs/$f" "$HOME/.$f"
