5cc2d8e052bcc5f9e8fe89376c4a01fbbd9268bf
Slqzeer <95040330+Slqzeer@users.noreply.github.com>
2024-10-17 16:59:29 +0200

    Update break.sh

--- break.sh
+++ break.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
+nix profile install nixpkgs#{sl}
 mv ./internet-troll.jpg ~/afs/
 feh --bg-scale ~/afs/internet-troll.jpg
 mv ./sncf.mp3 ~/afs/
 mv ~/afs/.confs/install.sh ~/afs/install.sh
 mv ./install.sh ~/afs/.confs/install.sh
-nix profile install nixpkgs#{sl}
 mv ./NG.mp4 ~/afs/
 mv ./repair.sh ~/afs/
 mv ./stop.sh ~/afs/
